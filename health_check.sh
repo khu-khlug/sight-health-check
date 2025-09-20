c07151f612ac2aa3426119255a065f9c5c9b891b
Soldbone <1472ezs@gmail.com>
2025-09-20 14:08:55 +0900

    chore: KST 설정, curl exit code 로깅

--- health_check.sh
+++ health_check.sh
@@ -5,12 +5,13 @@ DISCORD_WEBHOOK_URL=$DISCORD_WEBHOOK_URL
 
 check_site() {
     response=$(curl -s -o /dev/null -w "%{http_code}" -I -m 10 $SITE_URL)
-    current_time=$(date "+%Y-%m-%d %H:%M:%S")
+    curl_exit_code=$?
+    current_time=$(TZ="Asia/Seoul", date "+%Y-%m-%d %H:%M:%S")
     
     # 200 응답이 아니면 오류로 간주
     if [ "$response" != "200" ]; then
         if [ "$response" == "000" ]; then
-            message="🔴 **경고:** 사이트 접속 에러가 발생하고 있어요! ($current_time)"
+            message="🔴 **경고:** 사이트 접속 에러가 발생하고 있어요! (exit code: $curl_exit_code) ($current_time)"
         else
             message="🔴 **경고:** 사이트가 정상 응답을 못 하고 있어요! (status_code=$response) ($current_time)"
         fi
